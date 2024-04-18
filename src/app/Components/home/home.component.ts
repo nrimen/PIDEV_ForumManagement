ad85117319962a1a8dea73830bc061e5144b1e73
nour <nourelimen.hassine@esprit.tn>
2024-04-18 10:01:46 +0100

    updating home page

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,32 +1,34 @@
-
-import {Event} from "../../Core/Modules/Event-Model/event/event";
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Event } from "../../Core/Modules/Event-Model/event/event";
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 import { EventServiceService } from '../../Core/Services/EventServices/event-service.service';
 import { Subscription, interval } from 'rxjs';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   event: Event | undefined;
-
   daysLeft: number = 0;
   hoursLeft: number = 0;
   minutesLeft: number = 0;
   secondsLeft: number = 0;
   private countdownSubscription: Subscription | undefined;
+  private mySwiper: Swiper | undefined;
 
   constructor(private eventService: EventServiceService) {}
 
   ngOnInit() {
     this.eventService.getEventsList().subscribe(
       (events: Event[]) => {
+        console.log('Events:', events);
         if (events && events.length > 0 && events[0].debutDate) {
           const eventDate = new Date(events[0].debutDate);
           this.updateCountdown(eventDate);
           this.startCountdownUpdateInterval(eventDate);
+          this.event = events[0]; // Assign the first event to the event property
         } else {
           // Handle the case where events list is empty or debutDate is null
           console.error('Events list is empty or debutDate is null or undefined.');
@@ -38,8 +40,32 @@ export class HomeComponent implements OnInit, OnDestroy {
     );
   }
 
+  ngAfterViewInit() {
+    this.initSwiper();
+  }
+
   ngOnDestroy() {
     this.stopCountdownUpdateInterval();
+    this.destroySwiper();
+  }
+
+  initSwiper() {
+    this.mySwiper = new Swiper('.swiper-container', {
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
+
+  destroySwiper() {
+    if (this.mySwiper) {
+      this.mySwiper.destroy();
+    }
   }
 
   updateCountdown(eventDate: Date) {
